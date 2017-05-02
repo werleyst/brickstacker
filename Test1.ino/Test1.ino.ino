0847dde503fcd73decbeb5e285e22d7610ef2bd0
werleyst <werleyst@miamioh.edu>
2017-05-02 17:38:09 -0400

    fixed bugs that showed up with testing

--- Test1.ino/Test1.ino.ino
+++ Test1.ino/Test1.ino.ino
@@ -30,9 +30,9 @@ uint16_t yellow = matrix.Color444(15, 15, 0);
 //_______________________________________________________________________________________________________
 //Global variables
 int button = 52;
-int boxWidth = 8;
+int boxWidth = 16;
 int boxHeight = 4;
-int level = 1;
+int level = 0;
 Box *boxes[8]; //array of each layer of boxes
   
 
@@ -40,15 +40,19 @@ Box *boxes[8]; //array of each layer of boxes
 void setup() {
   pinMode(button, INPUT);
   matrix.begin();
+  matrix.fillScreen(matrix.Color333(0,0,0));
+  level=0;
   for(int i=0; i<8; i++){
-    boxes[i] =  new Box(12,28-i*4,boxWidth);
+    boxes[i] =  new Box(rand()%32,28-i*4,boxWidth);
   }
+  boxes[0]->dir = (rand()%2==0?-1:1); //boxes start in random position an direction
   drawBox(boxes[0]);
+  delay(500);
 }
 
 //Cycle for gameplay
 void loop() {
-  cycleBox(boxes[level%8],min(level+3,10)); //moves current box back and forth
+  cycleBox(boxes[level%8],min(level+2,20)); //moves current box back and forth
   checkButton(); //checks for button press
 }
 
@@ -62,30 +66,34 @@ void checkButton(){
     if(level==0){
       delay(1000);
       level++;
-      drawBox(boxes[level]);
+      drawBox(boxes[1]);
+      boxes[1]->dir = (rand()%2==0?-1:1);
     }
 
     //resets screen after top layer, starts at bottom again
     else if(level%8==7){
       checkAlign();
+      delay(500);
 
       matrix.fillScreen(matrix.Color333(0,0,0)); //reset screen
       boxes[0]->xpos = boxes[7]->xpos;
       boxes[0]->width = boxes[7]->width;
       boxes[1]->width = boxes[0]->width;
-      boxes[1]->xpos = 28;
-      boxes[1]->dir = 1;
-      delay(500);
+      boxes[1]->xpos = rand()%32;
+      boxes[1]->dir = (rand()%2==0?-1:1); //boxes start in random position an direction
+
+      level++;
       drawBox(boxes[0]);
       drawBox(boxes[1]);
-      level++;
     }
 
     //normal level, not first or top of screen
     else{
       checkAlign();
       delay(500);
+      boxes[level%8]->xpos = rand()%32;
       drawBox(boxes[level%8]);
+      boxes[level%8]->dir = (rand()%2==0?-1:1);//boxes start in random position an direction
     }
   }
 }
@@ -98,26 +106,25 @@ void checkAlign(){
   int width = boxes[level%8]->width;
   Box *fall;
 
-  if(abs(error)>width){
+  if(abs(error)>=width){ //complete miss
+    delay(1000);
     loseGame();
-  }
-  
-  else if(error < 0){
-    boxes[level%8]->xpos+=error;
-    boxes[level%8]->width-=error;
-    fall = new Box(xpos,ypos,error);
+    
+  }else if(error < 0){ //error on left side
+    boxes[level%8]->xpos+=abs(error);
+    boxes[level%8]->width-=abs(error);
+    fall = new Box(xpos,ypos,abs(error));
     boxFall(fall);
-  }
-  
-  else if(error > 0){
+    
+  }else if(error > 0){ //error on ride side
     boxes[level%8]->width-=error;
     fall = new Box(xpos+width-error,ypos,error);
     boxFall(fall);
   }
   
   level++;
-  boxes[level%8]->width=width-error;
-  boxes[level%8]->xpos=28;
+  boxes[level%8]->width=width-abs(error);
+  boxes[level%8]->xpos=rand()%32;
 }
 
 
@@ -149,7 +156,7 @@ void cycleBox(Box *b, int speed){
     b->dir = -b->dir;
   }
   moveBox(b);
-  delay(100.0/speed);
+  delay(200.0/speed);
 }
 
 //used for making piece of box fall off the screen
@@ -169,17 +176,19 @@ void loseGame(){
   matrix.fillScreen(matrix.Color333(0, 0, 0));
   
   // draw some text!
-  matrix.setCursor(1, 0);    // start at top left, with one pixel of spacing
+  matrix.setCursor(0, 0);    // start at top left, with one pixel of spacing
   matrix.setTextSize(1);     // size 1 == 8 pixels high
   //matrix.setTextWrap(false); // Don't wrap at end of line - will do ourselves
 
   matrix.setTextColor(matrix.Color333(7,7,7));
   matrix.println("Game");
-  matrix.setCursor(1, 9);
+  matrix.setCursor(0, 8);
   matrix.println("Over!");
-  matrix.setCursor(1,18);
-  matrix.println("Score:"+level);
-  delay(100000000);
+  matrix.setCursor(0,16);
+  matrix.print("Lv");
+  matrix.setCursor(14,16);
+  matrix.println(level);
+  delay(1000000);
 }
 
 
