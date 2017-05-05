41d9675f553e05a5437c5797eb1d40198be7cd07
Landreej <landreej@miamioh.edu>
2017-05-04 22:44:58 -0400

    Final, Completed Code. Added MP3 playback for theme music, button press sound, and end of game sound. Also added hard reset button.

---
+++ Working-Final-Sound/Working-Final-Sound.ino
@@ -0,0 +1,128 @@
+/*************************************************** 
+  This is an example for the Adafruit VS1053 Codec Breakout
+
+  Designed specifically to work with the Adafruit VS1053 Codec Breakout 
+  ----> https://www.adafruit.com/products/1381
+
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+// include SPI, MP3 and SD libraries
+#include <SPI.h>
+#include <Adafruit_VS1053.h>
+#include <SD.h>
+
+// These are the pins used for the music maker shield
+#define SHIELD_RESET  -1      // VS1053 reset pin (unused!)
+#define SHIELD_CS     7      // VS1053 chip select pin (output)
+#define SHIELD_DCS    6      // VS1053 Data/command select pin (output)
+
+// These are common pins between breakout and shield
+#define CARDCS 4     // Card chip select pin
+// DREQ should be an Int pin, see http://arduino.cc/en/Reference/attachInterrupt
+#define DREQ 3       // VS1053 Data request, ideally an Interrupt pin
+
+Adafruit_VS1053_FilePlayer musicPlayer = 
+  // create shield-example object!
+  Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
+
+
+////
+
+int button1 = 5;
+int button2 = 8;
+int button3 = 9;
+
+/// File listing helper
+void printDirectory(File dir, int numTabs) {
+   while(true) {
+     
+     File entry =  dir.openNextFile();
+     if (! entry) {
+       // no more files
+       //Serial.println("**nomorefiles**");
+       break;
+     }
+     for (uint8_t i=0; i<numTabs; i++) {
+       Serial.print('\t');
+     }
+     Serial.print(entry.name());
+     if (entry.isDirectory()) {
+       Serial.println("/");
+       printDirectory(entry, numTabs+1);
+     } else {
+       // files have sizes, directories do not
+       Serial.print("\t\t");
+       Serial.println(entry.size(), DEC);
+     }
+     entry.close();
+   }
+}
+
+void setup() {
+  pinMode(A2, INPUT);
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+  pinMode(button3, INPUT);
+  
+  Serial.begin(9600);
+  Serial.println("Adafruit VS1053 Library Test");
+
+  // initialise the music player
+  if (! musicPlayer.begin()) { // initialise the music player
+     Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
+     while (1);
+  }
+  Serial.println(F("VS1053 found"));
+
+  //musicPlayer.sineTest(0x44, 500);    // Make a tone to indicate VS1053 is working
+ 
+  if (!SD.begin(CARDCS)) {
+    Serial.println(F("SD failed, or not present"));
+    while (1);  // don't do anything more
+  }
+  Serial.println("SD OK!");
+  
+  // list files
+  printDirectory(SD.open("/"), 0);
+  
+  // Set volume for left, right channels. lower numbers == louder volume!
+  musicPlayer.setVolume(40,40);
+  
+
+  /***** Two interrupt options! *******/ 
+  // This option uses timer0, this means timer1 & t2 are not required
+  // (so you can use 'em for Servos, etc) BUT millis() can lose time
+  // since we're hitchhiking on top of the millis() tracker
+  //musicPlayer.useInterrupt(VS1053_FILEPLAYER_TIMER0_INT);
+  
+  // This option uses a pin interrupt. No timers required! But DREQ
+  // must be on an interrupt pin. For Uno/Duemilanove/Diecimilla
+  // that's Digital #2 or #3
+  // See http://arduino.cc/en/Reference/attachInterrupt for other pins
+  // *** This method is preferred
+  if (! musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT))
+    Serial.println(F("DREQ pin is not an interrupt pin"));
+
+  musicPlayer.startPlayingFile("theme.mp3");
+}
+
+void loop() {  
+   while(musicPlayer.playingMusic){
+    if (digitalRead(button2) == HIGH){
+      delay(10);
+      musicPlayer.startPlayingFile("block.mp3");
+      delay(300);
+      musicPlayer.startPlayingFile("theme.mp3");
+     }
+     if (digitalRead(button3) == HIGH){
+        musicPlayer.startPlayingFile("death.mp3");
+        delay(1000);
+      }
+   }
+}
